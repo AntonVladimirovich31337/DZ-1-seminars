8b870a556e626b72cfa9019ba2a1e8f179eea9a3
AntonAir <toshik0304@rambler.ru>
2023-03-11 16:53:23 +0300

    dz-1 task 2

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿//dz2
+
+int a, b;
+Console.Write($"Введите число а: ");
+int.TryParse(Console.ReadLine()!, out a);
+Console.Write($"Введите число b: ");
+int.TryParse(Console.ReadLine()!, out b);
+
+if (b > a)
+    Console.Write($"max = {b} ");
+else
+    Console.Write($"min = ");
+
+
