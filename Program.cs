03dab51c8e112931583c9f299ff9e4b92c352c5c
AntonAir <toshik0304@rambler.ru>
2023-03-12 12:03:56 +0300

    add branch task4

--- Program.cs
+++ Program.cs
@@ -1,15 +1,17 @@
 ﻿
 
-int a, b;
+int a, b, c;
+
 Console.Write($"Введите число а: ");
 int.TryParse(Console.ReadLine()!, out a);
 Console.Write($"Введите число b: ");
 int.TryParse(Console.ReadLine()!, out b);
+Console.Write($"Введите число c: ");
+int.TryParse(Console.ReadLine()!, out c);
 
-if (b > a)
+if (a > b && a > c)
+    Console.Write($"max = {a} ");
+if (b > a && b > c)
     Console.Write($"max = {b} ");
-else
-    Console.Write($"min = ");
-
-
-
+if (c > a && c > a)
+    Console.Write($"max = {c} ");
